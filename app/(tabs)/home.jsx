6d2861883751486336d069ea91311e5c072d29f7
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-06-03 22:01:37 +0100

    started working on homepage

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import image from "../../constants/images";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="h-[100%] w-[100%] bg-primary px-6">
+      <View className="flex-row justify-between items-center border-red-700 w-[100%] mt-16">
+        <View className="flex gap-2">
+          <Text className="capitalize text-gray-100 text-base">
+            welcome back
+          </Text>
+          <Text className="capitalize text-white text-xl font-pbold ">
+            jsmastery
+          </Text>
+        </View>
+        <View>
+          <Image
+            source={image.logoSmall}
+            className="w-[40px] h-[40px]"
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
